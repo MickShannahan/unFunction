ec648acc8139ccf6250eeaeaa2399464316904fb
mick shannahan <>
2022-11-09 08:30:45 -0700

    some error handling

--- blobber/index.mjs
+++ blobber/index.mjs
@@ -2,15 +2,19 @@ import { BlobServiceClient } from "@azure/storage-blob";
 import multipart from 'parse-multipart'
 export default async function (context, req) {
     const blobStorage = BlobServiceClient.fromConnectionString(process.env.AZURE_BLOB_CONNECTION)
-
     context.log('Uploading to blob: ', req.query.container);
     try {
+        if (!req.query.container) throw new Error('no container specified, specify in query')
+        if (!req.query.fileName) throw new Error('no fileName specified, specify in query')
+
         const containerName = req.query.container
         const fileName = req.query.fileName
         // parse multipart
         const bodyBuffer = Buffer.from(req.body);
         const boundary = multipart.getBoundary(req.headers["content-type"]);
         const parts = multipart.Parse(bodyBuffer, boundary);
+
+        if (parts.length <= 0) throw new Error('no File attached')
         const file = parts[0]
 
         const container = blobStorage.getContainerClient(containerName)
