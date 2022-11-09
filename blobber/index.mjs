ccacddf643480d85c1ac2d2d0cfa6e73371ec621
mick shannahan <>
2022-11-09 08:08:23 -0700

    deploy?

--- blobber/index.mjs
+++ blobber/index.mjs
@@ -1,23 +1,23 @@
 import { BlobServiceClient } from "@azure/storage-blob";
 import multipart from 'parse-multipart'
 export default async function (context, req) {
-    // const blobStorage = BlobServiceClient.fromConnectionString(process.env.AZURE_BLOB_CONNECTION)
+    const blobStorage = BlobServiceClient.fromConnectionString(process.env.AZURE_BLOB_CONNECTION)
 
     context.log('Uploading to blob: ', req.query.container);
     try {
-        // const containerName = req.query.container
-        // const fileName = req.query.fileName
-        // // parse multipart
-        // const bodyBuffer = Buffer.from(req.body);
-        // const boundary = multipart.getBoundary(req.headers["content-type"]);
-        // const parts = multipart.Parse(bodyBuffer, boundary);
-        // const file = parts[0]
+        const containerName = req.query.container
+        const fileName = req.query.fileName
+        // parse multipart
+        const bodyBuffer = Buffer.from(req.body);
+        const boundary = multipart.getBoundary(req.headers["content-type"]);
+        const parts = multipart.Parse(bodyBuffer, boundary);
+        const file = parts[0]
 
-        // const container = blobStorage.getContainerClient(containerName)
+        const container = blobStorage.getContainerClient(containerName)
 
-        // const blockBlob = container.getBlockBlobClient(fileName)
+        const blockBlob = container.getBlockBlobClient(fileName)
 
-        // const response = await blockBlob.upload(file.data, file.data.length)
+        const response = await blockBlob.upload(file.data, file.data.length)
         return {
             status: 200,
             url: 'worked'
